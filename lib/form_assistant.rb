983f577c6fbeae0d95ef114334c83b47bf2eabb1
rpheath <rpheath@gmail.com>
2008-10-29 14:45:03 -0400

    removed the need to pass around the @template

--- lib/form_assistant.rb
+++ lib/form_assistant.rb
@@ -9,28 +9,28 @@ module RPH
         Collector.new.collect(tag)
       end
       
-      def element(e, attrs, content, template, binding = nil)
-        wrap(e.to_sym).having(attrs).around(content).for(template, binding)
+      def element(e, attrs, content, binding = nil)
+        wrap(e.to_sym).having(attrs).around(content).for(@template, binding)
       end
       
     public
       def div(attrs = {}, &block)
-        element(:div, attrs, @template.capture(&block), @template, block.binding)
+        element(:div, attrs, @template.capture(&block), block.binding)
       end
       
       def p(attrs = {}, &block)
-        element(:p, attrs, @template.capture(&block), @template, block.binding)
+        element(:p, attrs, @template.capture(&block), block.binding)
       end
       
       def submission(value = 'Save Changes', options = {})
-        element(:p, options.delete(:attrs), self.submit(value, options), @template)
+        element(:p, options.delete(:attrs), self.submit(value, options))
       end
     
       def method_missing(method, *args, &block)
         super(method, *args) unless block_given?
       
         css = { :class => method.to_s.downcase.gsub('_', '-') }
-        element(:div, css, @template.capture(&block), @template, block.binding)
+        element(:div, css, @template.capture(&block), block.binding)
       end
   end
 end
