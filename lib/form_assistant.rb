9ab6a620de627173215675331b9e30c550b73a2b
rpheath <rpheath@gmail.com>
2008-10-29 14:13:41 -0400

    minor refactoring

--- lib/form_assistant.rb
+++ lib/form_assistant.rb
@@ -9,20 +9,28 @@ module RPH
         Collector.new.collect(tag)
       end
       
+      def element(e, attrs, content, template, binding = nil)
+        wrap(e.to_sym).having(attrs).around(content).for(template, binding)
+      end
+      
     public
       def div(attrs = {}, &block)
-        wrap(:div).having(attrs).around(@template.capture(&block)).for(@template, block.binding)
+        element(:div, attrs, @template.capture(&block), @template, block.binding)
+      end
+      
+      def p(attrs = {}, &block)
+        element(:p, attrs, @template.capture(&block), @template, block.binding)
       end
       
       def submission(value = 'Save Changes', options = {})
-        wrap(:p).having(options.delete(:attrs)).around(self.submit(value, options)).for(@template)
+        element(:p, options.delete(:attrs), self.submit(value, options), @template)
       end
     
       def method_missing(method, *args, &block)
         super(method, *args) unless block_given?
       
-        css = method.to_s.downcase.gsub('_', '-')
-        wrap(:div).having(:class => css).around(@template.capture(&block)).for(@template, block.binding)
+        css = { :class => method.to_s.downcase.gsub('_', '-') }
+        element(:div, css, @template.capture(&block), @template, block.binding)
       end
   end
 end
