9b8c412652db873ff591994a19d5133b14852011
rpheath <rpheath@gmail.com>
2008-10-29 13:33:58 -0400

    cleanup again

--- lib/form_assistant/collector.rb
+++ lib/form_assistant/collector.rb
@@ -10,20 +10,21 @@ module RPH
           @items << item and return self
         end
         
-        def collection
-          @items
-        end
-        
         def having(attrs = {})
           collect(attrs)
         end
         
-        def around(content = nil, &block)
+        def around(content)
           collect(content)
         end
         
         def for(template, binding = nil)
           Builder.build(collection).for(template, binding)
+        end
+        
+      private
+        def collection
+          @items
         end
       end
   end
