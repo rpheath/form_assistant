53f05f8af3dd8089388f0cc088f6ff093cbce914
rpheath <rpheath@gmail.com>
2008-10-31 12:47:14 -0400

    who am I kidding - this is for another day

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,3 @@ require 'rubygems'
 require 'action_view'
 require 'action_controller'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'form_assistant')
-
-ActionView::Helpers::FormBuilder.send :include, RPH::FormAssistant
-
-class Project
-end
