128c347b62a7728e209f0496420a189fe907e160
rpheath <rpheath@gmail.com>
2008-11-07 15:32:17 -0500

    major refactoring: now all field formatting uses partials - TODO: cleanup the crap and update README

--- lib/form_assistant.rb
+++ lib/form_assistant.rb
@@ -4,38 +4,81 @@ end
 
 # Developed by Ryan Heath (http://rpheath.com)
 module RPH
-  # FormAssistant is currently made up of two custom FormBuilder's:
-  #
-  #   FormAssistant::FormBuilder
-  #     - provides several convenient helpers (see helpers.rb)
-  #     - method_missing hook to wrap content "on the fly"
-  #     - labels automatically attached to field helpers
-  #
-  #   FormAssistant::InlineErrorFormBuilder
-  #     - inherits from FormBuilder, so has all of the above functionality
-  #     - provides errors inline with the field that wraps it
-  #     - uses partials (in views/forms/) to style fields
-  #
   # The idea is to make forms extremely less painful and a lot more DRY
   module FormAssistant
-    # custom form builder
+    # FormAssistant::FormBuilder
+    #   * provides several convenient helpers (see helpers.rb)
+    #   * method_missing hook to wrap content "on the fly"
+    #   * labels automatically attached to field helpers
+    #   * format fields using partials (extremely extensible)
     # 
     # <% form_for @project, :builder => RPH::FormAssistant::FormBuilder do |form| %>
-    #   // form stuff
+    #   // fancy form stuff
     # <% end %>
     #
-    # Note: see #form_assistant_for() below for an easier way to use this builder
+    # - or -
+    # 
+    # <% form_assistant_for @project do |form| %>
+    #   // fancy form stuff
+    # <% end %>
     class FormBuilder < ActionView::Helpers::FormBuilder
       include RPH::FormAssistant::Helpers
-      class_inheritable_accessor :wrap_fields_with_paragraph_tag
+      cattr_accessor :ignore_templates
+      attr_accessor_with_default :fallback_template, 'field'
       
-      # set to true if you want the <label> and the <field>
-      # to be automatically wrapped in a <p> tag
-      # 
-      # Note: this can be set in config/initializers/form_assistant.rb ...
-      #   RPH::FormAssistant::FormBuilder.wrap_fields_with_paragraph_tag = true
-      self.wrap_fields_with_paragraph_tag = false
+      # if set to true, none of the templates in app/views/forms/ will
+      # be used; however, labels will still be automatically attached
+      # and all FormAssistant helpers are still avaialable
+      self.ignore_templates = false
       
+      # override the field_error_proc so that it no longer wraps the field
+      # with <div class="fieldWithErrors">...</div>, but just returns the field
+      ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+      
+    private
+      # get the error messages (if any) for a field
+      def error_message_for(field)
+        return nil unless has_errors?(field)
+        errors = object.errors[field]
+        [field.to_s.humanize, (errors.is_a?(Array) ? errors.to_sentence : errors).to_s].join(' ')
+      end
+      
+      # returns true if a field is invalid or the object is missing
+      def has_errors?(field)
+        !(object.nil? || object.errors[field].blank?)
+      end
+      
+      # checks to make sure the template exists
+      def template_exists?(template)
+        File.exists?(File.join(Rails.root, '/app/views/forms', '_' + template + '.html.erb'))
+      end
+      
+    protected
+      # render the appropriate partial based on whether or not
+      # the field has any errors
+      def render_partial_for(field, label, tip, template, args, include_errors = true)
+        @template.capture do
+          errors = include_errors ? error_message_for(field) : nil
+          locals = { :field => field, :label => label, :errors => errors, :tip => tip }
+          
+          # render the appropriate partial from app/views/forms/
+          @template.render :partial => "forms/#{template}", :locals => locals
+        end
+      end
+      
+      def render_field_with_label(element, field, name, options)
+        @template.capture do
+          # consider trailing labels
+          label_with_field = if %w(check_box radio_button).include?(name)
+            label_options = options.delete(:label)
+            label_options[:class] = (label_options[:class].to_s + ' inline').strip
+            element + label(field, label_options.delete(:text), label_options)
+          else
+            label(field, options[:label].delete(:text), options.delete(:label)) + element
+          end
+        end
+      end
+    
     public
       # redefining all traditional form helpers so that they
       # behave the way FormAssistant thinks they should behave
@@ -43,7 +86,7 @@ module RPH
         define_method name do |field, *args|
           # pull out the options
           options = args.detect { |arg| arg.is_a?(Hash) } || {}
-          
+
           options[:label] ||= {}
           # allow for a more convenient way to set common label options
           %w(id class text).each do |option|
@@ -51,76 +94,13 @@ module RPH
             options[:label].merge!(option.to_sym => options.delete(label_option)) if options[label_option]
           end
           
-          # consider trailing labels
-          label_with_field = if %w(check_box radio_button).include?(name)
-            label_options = options.delete(:label)
-            label_options[:class] = (label_options[:class].to_s + ' inline').strip
-            super + label(field, label_options.delete(:text), label_options)
-          else
-            label(field, options[:label].delete(:text), options.delete(:label)) + super
-          end
+          template = options.delete(:template) || name.to_s
+          template ||= 'trailing_label_field' if %w(check_box radio_button).include?(name)
+          template = self.fallback_template unless template_exists?(template)
+                      
+          return render_field_with_label(super, field, name, options) if self.class.ignore_templates || template.blank?
           
-          # return the fields
-          self.class.wrap_fields_with_paragraph_tag ? @template.content_tag(:p, label_with_field) : label_with_field
-        end
-      end
-    end
-    
-    # extends the custom form builder above to attach form errros to 
-    # the fields themselves, avoiding the <%= error_messages_for :object %> way
-    # 
-    # <% form_for @project, :builder => RPH::FormAssistant::InlineErrorFormBuilder do |form| %>
-    #   // form stuff
-    # <% end %>
-    #
-    # Note: see #inline_error_form_assistant_for() below for an easier way to use this builder
-    class InlineErrorFormBuilder < FormBuilder
-      # override the field_error_proc so that it no longer wraps the field
-      # with <div class="fieldWithErrors">...</div>, but just returns the field
-      ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| html_tag }
-      
-      # ensure that fields are NOT wrapped with <p> tags 
-      # (handle markup formatting in forms/_partials)
-      self.wrap_fields_with_paragraph_tag = false
-      
-    private
-      # get the error messages (if any) for a field
-      def error_message(field)
-        return unless has_errors?(field)
-        errors = object.errors.on(field)
-        errors.is_a?(Array) ? errors.to_sentence : errors
-      end
-      
-      # returns true if a field is invalid or the object is missing
-      def has_errors?(field)
-        !(object.nil? || object.errors.on(field).blank?)
-      end
-    
-    public
-      # redefining the custom FormBuilder's methods
-      send(:form_helpers).each do |name|
-        define_method name do |field, *args|
-          # use partials located in app/views/forms to
-          # handle the representation of the fields
-          render_partial_for(field) { super }
-        end
-      end
-      
-      # render the appropriate partial based on whether or not
-      # the field has any errors
-      def render_partial_for(field)
-        @template.capture do
-          # get the field/label combo provided by FormBuilder
-          locals = { :field_with_label => yield }
-          
-          # determine which partial to render based on if
-          # the field has any errors associated with it
-          if has_errors?(field)
-            locals.merge! :errors => error_message(field)
-            @template.render :partial => 'forms/field_with_errors', :locals => locals
-          else
-            @template.render :partial => 'forms/field_without_errors', :locals => locals
-          end
+          render_partial_for super, label(field, options[:label].delete(:text), options.delete(:label)), options.delete(:tip), template, args
         end
       end
     end
@@ -141,19 +121,10 @@ module RPH
         # easy way to make use of FormAssistant::FormBuilder
         #
         # <% form_assistant_for @project do |form| %>
-        #   // form stuff
+        #   // fancy form stuff
         # <% end %>
         def form_assistant_for(record_or_name_or_array, *args, &proc)
           form_for_with_builder(record_or_name_or_array, RPH::FormAssistant::FormBuilder, *args, &proc)
-        end
-      
-        # easy way to make use of FormAssistant::InlineErrorFormBuilder
-        #
-        # <% inline_error_form_assistant_for @project do |form| %>
-        #   // form stuff
-        # <% end %>
-        def inline_error_form_assistant_for(record_or_name_or_array, *args, &proc)
-          form_for_with_builder(record_or_name_or_array, RPH::FormAssistant::InlineErrorFormBuilder, *args, &proc)
         end
     end
   end   
