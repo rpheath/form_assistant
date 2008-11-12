582be57655556ca9185e34e36f61239a4c6eb13d
rpheath <rpheath@gmail.com>
2008-11-12 11:18:57 -0500

    now assuming a requirement at least rails 2.1.0

--- lib/form_assistant/rules.rb
+++ lib/form_assistant/rules.rb
@@ -6,8 +6,7 @@ module RPH
         # used mainly for #concat() so that this plugin will
         # work with versions of Rails other than edge
         def self.binding_required?
-          !!((Object.const_defined?(:Rails) && Rails.respond_to?(:version) ? 
-              Rails.version : RAILS_GEM_VERSION) < '2.2.0')
+          !!( Rails.version < '2.2.0' )
         end
       end
   end
